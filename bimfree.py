a4f78b82a2f2c3ca895a4161771fbd321345065b
ciceromayk <38115321+ciceromayk@users.noreply.github.com>
2025-08-08 14:49:00 -0300

    Update bimfree.py

--- bimfree.py
+++ bimfree.py
@@ -1,61 +1,25 @@
 import streamlit as st
-import os
-import base64
+import ifcopenshell
 
-# Configuração da página
-st.set_page_config(page_title="IFC to GLB Converter", page_icon=":building:", layout="wide")
+def load_ifc(file_content):
+    try:
+        ifc_file = ifcopenshell.file(schema="IFC4")
+        ifc_file.read(file_content)
+        return ifc_file
+    except:
+        st.error("Erro ao ler o arquivo IFC")
 
-st.sidebar.title("Visualizador IFC")
-st.sidebar.markdown("Faça upload do seu arquivo IFC para visualizar.")
+st.title("Visualização de Arquivos IFC Online")
 
-uploaded_file = st.sidebar.file_uploader("Escolha um arquivo IFC", type=["ifc"])
+uploaded_file = st.file_uploader("Escolha um arquivo IFC", type=["ifc"])
 
-# Verifica se um arquivo foi carregado
 if uploaded_file is not None:
-    st.sidebar.success("Arquivo carregado com sucesso!")
-    
-    # Lê o arquivo carregado e encode em base64
-    file_content = uploaded_file.getvalue()
-    encoded_file = base64.b64encode(file_content).decode('utf-8')
-    file_name = uploaded_file.name
-
-    # Estrutura HTML/JavaScript usando Blob para carregar o IFC
-    viewer_html_code = f"""
-    <html>
-    <head>
-        <script type="module">
-            import {{ IfcViewerAPI }} from 'https://cdn.jsdelivr.net/npm/@ifcjs/viewer@3.0.0/dist/index.js';
-
-            const container = document.getElementById('viewer');
-            const viewer = new IfcViewerAPI({{ container, backgroundColor: new THREE.Color(0xffffff) }});
-            viewer.addAxes();
-            viewer.addGrid();
-
-            async function loadIfc() {{
-                const response = await fetch('data:application/octet-stream;base64,{encoded_file}');
-                const ifcBlob = await response.blob();
-                const ifcURL = URL.createObjectURL(ifcBlob);
-                await viewer.IFC.loadIfcUrl(ifcURL);
-                viewer.fitToFrame();
-            }}
-
-            loadIfc();
-        </script>
-        <style>
-            #viewer {{
-                width: 100%;
-                height: 800px;
-                border: 1px solid #ccc;
-            }}
-        </style>
-    </head>
-    <body>
-        <div id="viewer"></div>
-    </body>
-    </html>
-    """
-
-    # Renderiza o HTML
-    st.components.v1.html(viewer_html_code, height=800)
+    model = load_ifc(uploaded_file.getvalue())
+    if model:
+        projects = model.by_type("IfcProject")
+        if projects:
+            st.write("Nome do Projeto:", projects[0].Name)
+        else:
+            st.error("Nenhum projeto encontrado no arquivo.")
 else:
-    st.sidebar.warning("Por favor, carregue um arquivo IFC.")
+    st.info("Por favor, faça upload de um arquivo IFC.")
